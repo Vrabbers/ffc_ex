77d66c8bdcd1bb4d8980d555aa2e9c087af16582
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-02-03 00:02:37 -0300

    pretty game start embed (phew)

--- lib/ffc_ex/game.ex
+++ lib/ffc_ex/game.ex
@@ -1,11 +1,14 @@
 defmodule FfcEx.Game do
   # Doesn't make much sense to try restarting a crashed game
   use GenServer, restart: :temporary
+  alias Nostrum.Struct.Embed
+  alias Nostrum.Struct.Embed.Thumbnail
   alias FfcEx.DmCache
   alias Nostrum.Api
   alias FfcEx.Lobby
   alias FfcEx.Game
   alias Nostrum.Struct.User
+  require Logger
 
   @enforce_keys [:id, :players, :spectators]
   defstruct @enforce_keys
@@ -50,6 +53,18 @@ defmodule FfcEx.Game do
       end
 
     if Enum.all?(responses, fn {{resp, _}, _} -> resp == :ok end) do
+      embed =
+        %Embed{
+          title: "Final Fantastic Card",
+          description: """
+          Welcome to Final Fanstastic Card!
+          """,
+          color: Application.fetch_env!(:ffc_ex, :color),
+          thumbnail: %Thumbnail{url: "attachment://draw.png"}
+        }
+        |> put_id_footer(game)
+
+      broadcast(game, embeds: [embed], files: ["./img/draw.png"])
       {:reply, :ok, game}
     else
       {:stop, :error, {:cannot_dm, for({{:error, _}, user} <- responses, do: user)}, game}
@@ -59,5 +74,37 @@ defmodule FfcEx.Game do
   @spec participants(Game.t()) :: [User.id()]
   defp participants(game) do
     game.players ++ game.spectators
+  end
+
+  defp broadcast(game, message) do
+    broadcast_to(participants(game), message)
+  end
+
+  defp broadcast_to(users, message) do
+    for user <- users do
+      tell(user, message)
+    end
+
+    :ok
+  end
+
+  defp tell(user, message) do
+    {:ok, dm_channel} = DmCache.create(user)
+
+    Task.start(fn ->
+      case Api.create_message(dm_channel, message) do
+        {:error, error} ->
+          Logger.error(
+            "Error telling #{inspect(user)} message: #{inspect(message)}: #{inspect(error)}"
+          )
+
+        {:ok, _} ->
+          :noop
+      end
+    end)
+  end
+
+  defp put_id_footer(embed, game) do
+    Embed.put_footer(embed, "Game \##{game.id}")
   end
 end
