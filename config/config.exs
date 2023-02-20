b0ea713a18b92dd8310bbeb7e7386f42f4633f0d
Vrabbers <25323861+Vrabbers@users.noreply.github.com>
2023-02-20 19:10:25 -0300

    different prefix and color for dev version

--- config/config.exs
+++ config/config.exs
@@ -4,8 +4,8 @@ config :logger,
   level: :info
 
 config :ffc_ex,
-  prefix: "ffc:",
-  color: 0x4251F5
+  prefix: if(Mix.env() == :prod, do: "ffc:", else: "ffd:"),
+  color: if(Mix.env() == :prod, do: 0x4251F5, else: 0xFFFFFF)
 
 config :nostrum,
   gateway_intents: [
